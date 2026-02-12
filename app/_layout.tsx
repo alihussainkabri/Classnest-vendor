550185a198851bf719f12b40483504ee3a04073f
webamplifier <webamplifier85@gmail.com>
2026-02-12 19:28:31 +0530

    ssetup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
@@ -16,16 +16,28 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    
+
     <GluestackUIProvider mode="dark">
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
-        <Stack.Screen name="Auth/Welcome" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+      <ThemeProvider value={DefaultTheme}>
+        <Stack screenOptions={{
+          headerShown: false
+        }}>
+          <Stack.Screen name="Auth/Welcome" options={{ presentation: 'modal', title: 'yes' }} />
+          <Stack.Screen name="Auth/Login" options={{ presentation: 'modal', title: 'yes' }} />
+          <Stack.Screen name="Auth/Verification" options={{ presentation: 'modal', title: 'yes' }} />
+          <Stack.Screen name="Auth/Success" options={{ presentation: 'modal', title: 'yes' }} />
+
+          <Stack.Screen name="Vendor/SetupScreen1" options={{ presentation: 'modal', title: 'yes' }} />
+          <Stack.Screen name="Vendor/SetupScreen2" options={{ presentation: 'modal', title: 'yes' }} />
+          <Stack.Screen name="Vendor/SelectCourses" options={{ presentation: 'modal', title: 'yes' }} />
+          <Stack.Screen name="Vendor/ReviewDetails" options={{ presentation: 'modal', title: 'yes' }} />
+          <Stack.Screen name="Vendor/DetailSuccess" options={{ presentation: 'modal', title: 'yes' }} />
+
+          <Stack.Screen name="Vendor/ListingClass1" options={{ presentation: 'modal', title: 'yes' }} />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
     </GluestackUIProvider>
-  
+
   );
 }
