a5403d62ff8907c7651d1d9aecbbeadaff329590
webamplifier <webamplifier85@gmail.com>
2026-03-09 15:56:08 +0530

    phone flag solved

--- app/Auth/Login.js
+++ app/Auth/Login.js
@@ -5,11 +5,13 @@ import {
     CheckboxLabel,
 } from '@/components/ui/checkbox';
 import { CheckIcon } from '@/components/ui/icon';
+import { Input, InputField } from '@/components/ui/input';
+import Entypo from '@expo/vector-icons/Entypo';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { router } from 'expo-router';
 import { useRef, useState } from 'react';
 import { Dimensions, ImageBackground, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import PhoneInput from "react-native-phone-number-input";
+import CountryPicker from "react-native-country-picker-modal";
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Toast } from 'toastify-react-native';
 import { colors, fonts } from '../../config/Config';
@@ -18,10 +20,16 @@ import { url } from '../../helpers';
 const Login = ({ navigation }) => {
     const [value, setValue] = useState('')
     const [countryCode, setCountryCode] = useState('IN');
+    const [callingCode, setCallingCode] = useState("91");
     const [checked, setChecked] = useState(false);
 
     const phoneInputRef = useRef(null);
     const inset = useSafeAreaInsets()
+
+    const onSelect = (country) => {
+        setCountryCode(country.cca2);
+        setCallingCode(country.callingCode[0]);
+    };
 
     async function generateOTP() {
         const formData = new FormData()
@@ -43,7 +51,7 @@ const Login = ({ navigation }) => {
                         pathname: 'Auth/Verification',
                         params: {
                             mobile_number: value,
-                            newly_created : data?.newly_created
+                            newly_created: data?.newly_created
                         }
                     })
                 }, 200);
@@ -72,30 +80,51 @@ const Login = ({ navigation }) => {
             <View style={{ marginHorizontal: 16, flex: 1, justifyContent: 'space-between' }}>
                 <View style={{}}>
                     <Text style={{ color: '#17181C', fontFamily: fonts.IntSB, fontSize: 16, marginTop: 26 }}>Phone Number</Text>
-                    <PhoneInput
+                    {/* <PhoneInput
                         key={countryCode}
                         ref={phoneInputRef}
                         defaultValue={value}
                         defaultCode={countryCode}
                         layout="first"
                         onChangeText={(text) => { setValue(text) }}
-                        // onChangeFormattedText={(text) => {
-                        //     setFormattedValue(text);
-                        // }}
-                        withShadow
+                        withShadow={false}
                         autoFocus
                         withFlag={true}
                         containerStyle={styles.phoneContainer}
                         textContainerStyle={styles.textInput}
                         countryPickerProps={{
-                            withEmoji: true,
+                            withEmoji: false,
                             withFlag: true,
                             withAlphaFilter: true,
                             withFilter: true,
+                            flagSize: 20,
+                            withFlagButton: true,
                         }}
                         placeholder='Enter 10-digit mobile number'
-                        flagButtonStyle={{ backgroundColor: '#E6EEFF', borderRadius: 16 }}
-                    />
+                        flagButtonStyle={{ backgroundColor: '#E6EEFF', borderRadius: 16, width: 70, justifyContent: 'center' }}
+                        renderDropdownImage={<Text style={{ fontSize: 16 }}>▼</Text>}
+                    /> */}
+                    <View style={[styles.phoneContainer, { flexDirection: 'row' }]}>
+                    <View style={styles.flagBox}>
+                        <CountryPicker
+                            countryCode={countryCode}
+                            withFilter
+                            withFlag
+                            withCallingCode={false}
+                            withEmoji={true}
+                            onSelect={onSelect}
+                        />
+                        <Entypo name="chevron-small-down" size={18} color="black" style={{marginLeft: -14}} />
+                    </View>
+                    <Input
+                        variant="none"
+                        size="lg"
+                        isRequired
+                        style={{ flex: 1 }}
+                    >
+                        <InputField value={value} keyboardType="phone-pad" onChangeText={setValue} placeholder="Enter 10-digit mobile number" style={{ color: '#787878', fontSize: 15, fontFamily: fonts.IntReg, paddingLeft: 16 }} />
+                    </Input>
+                </View>
                 </View>
 
                 <View style={{ alignItems: 'center' }}>
@@ -133,13 +162,23 @@ const styles = StyleSheet.create({
         width: '100%',
         height: Dimensions.get('window').height / 100 * 30,
     },
+    flagBox: {
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "#E6EEFF",
+        paddingHorizontal: 12,
+        borderRadius: 16,
+        height: 60,
+        marginLeft: -1
+    },
     phoneContainer: {
         width: '100%',
         height: 60,
         borderWidth: 1,
         borderColor: '#D9D9D9',
         borderRadius: 16,
-        marginTop: 20
+        marginTop: 20,
+        alignItems: 'center'
     },
     textInput: {
         paddingVertical: 0,
